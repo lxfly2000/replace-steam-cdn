147bbfb06e985622f95454ffb2f66a5bb87f857d
Yuxin <lxfly2000@outlook.com>
2024-03-29 11:52:06 +0800

    Update replace-steam-cdn.user.js

--- replace-steam-cdn.user.js
+++ replace-steam-cdn.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Replace Steam CDN
 // @namespace    https://github.com/lxfly2000/replace-steam-cdn/raw/master/replace-steam-cdn.user.js
-// @version      1.3.2
+// @version      1.3.3
 // @updateURL    https://github.com/lxfly2000/replace-steam-cdn/raw/master/replace-steam-cdn.user.js
 // @downloadURL  https://github.com/lxfly2000/replace-steam-cdn/raw/master/replace-steam-cdn.user.js
 // @description  Replace Steam CDN
@@ -19,7 +19,8 @@
 var substitutions=[
     {a:"community.cloudflare.steamstatic.com",b:"community.akamai.steamstatic.com"},
     {a:"avatars.cloudflare.steamstatic.com",b:"avatars.akamai.steamstatic.com"},
-    {a:"cdn.cloudflare.steamstatic.com",b:"media.st.dl.eccdnx.com"}
+    {a:"cdn.cloudflare.steamstatic.com",b:"media.st.dl.eccdnx.com"},
+    {a:"cdn.akamai.steamstatic.com",b:"media.st.dl.eccdnx.com"}
 ];
 var asyncloadHosts=[
     "steamdb.info"
